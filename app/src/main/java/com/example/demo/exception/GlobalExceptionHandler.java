26f86644ddfe458f82e64fbdc5b444a7751c4491
Thomas Boyer <thomas.boyer.dev@gmail.com>
2025-10-23 14:26:02 +0200

    add bad request response handler in GlobalExceptionHandler

--- app/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ app/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -12,7 +13,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleResourceNotFound(
             ResourceNotFoundException ex,
-            WebRequest request) {
+            WebRequest request
+    ) {
 
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                 HttpStatus.NOT_FOUND.value(),
@@ -23,11 +25,25 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBadRequestException(
+            Exception ex,
+            WebRequest request
+    ) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                "Votre requête n'est pas formatée correctement, veillez vous référer au contrat d'interface : http://exo-spring-boot.localhost/swagger-ui/index.html",
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDTO> handleGlobalException(
             Exception ex,
-            WebRequest request) {
-
+            WebRequest request
+    ) {
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "Une erreur interne s'est produite",
